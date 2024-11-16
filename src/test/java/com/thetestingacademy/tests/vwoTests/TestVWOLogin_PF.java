d94ce629a7f8168b674d51608aad4e250dbcd1e5
Promode <pramoddutta_96@yahoo.com>
2024-11-16 12:03:45 +0530

    Added Page Factory and Page Object Model

---
+++ src/test/java/com/thetestingacademy/tests/vwoTests/TestVWOLogin_PF.java
@@ -0,0 +1,28 @@
+package com.thetestingacademy.tests.vwoTests;
+
+import com.thetestingacademy.base.CommonToAllTest;
+import com.thetestingacademy.driver.DriverManager;
+import com.thetestingacademy.pages.pageFactory.LoginPage_PF;
+import com.thetestingacademy.utils.PropertyReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestVWOLogin_PF extends CommonToAllTest {
+
+
+    private static final Logger logger = LogManager.getLogger(TestVWOLogin_POM.class);
+
+    @Test
+    public void testLoginNegativeVWO_PF() {
+        logger.info("Starting the Testcases Page Factory");
+        WebDriver driver = DriverManager.getDriver();
+        driver.get(PropertyReader.readKey("url"));
+        LoginPage_PF loginPage_PF = new LoginPage_PF(driver);
+        String error_msg = loginPage_PF.loginToVWOInvalidCreds();
+        Assert.assertEquals(error_msg, PropertyReader.readKey("error_message"));
+    }
+}
